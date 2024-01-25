c6d0e554ec8f802c8e498b2b919b92ac3042fd0b
DESKTOP-ALP9J2M\Usuario <csi23-mserina@colegioaltair.net>
2024-01-25 19:54:01 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ejercicio3PesoC{ 
+
+    /// <summary>
+    /// Clase principal de programa
+    /// msm - 250124
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Metodo quecontiene el procedimiento de la aplicacion
+        /// msm - 250124
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int maximoPeso = 4000;
+            int pesoSaco;
+            Console.WriteLine("Inserte el peso del saco");
+            pesoSaco = Convert.ToInt32(Console.ReadLine());
+            int numerosSacos = maximoPeso / pesoSaco;
+            Console.WriteLine("Puedes cargar " + numerosSacos + " sacos" );
+
+        }
+    }
+}
